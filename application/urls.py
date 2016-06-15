0a266cce1a565891afa5e717beb642f9669592bb
togobananas <endpoo@gmail.com>
2016-06-16 02:01:30 +0300

    markup for index page and minimal views

--- application/urls.py
+++ application/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view()),
+    url(r'^sample1/$', views.Sample1View.as_view(), name='sample1'),
+    url(r'^sample2/$', views.Sample2View.as_view(), name='sample2'),
 ]
--- application/views.py
+++ application/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View, CreateView
+from django.http import JsonResponse, HttpResponse
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class Sample1View(CreateView):
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse(status=200)
+
+
+class Sample2View(CreateView):
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse(status=200)
